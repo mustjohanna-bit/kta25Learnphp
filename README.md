782d80ad53659ca3dd52985b4b89d95a2e9636a8
johannamust <mustjohanna@gmail.com>
2026-01-16 15:14:54 +0200

    Add types example

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # kta25Learnphp
 - algus tag peab alati olema
 - localhost on mul 8000
+- kõik muutujad algavad dollari ($) märgiga
+- php peab panema ;
+- // tähendab line commentit
+- \ saab ainult rootsi klaviatuuriga
+- $num = array(1, 2, 3, 4);
+$num = [1, 2, 3, 4]; (Need on samad)
+- std on standard
--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
-echo 'Hello World';
 
-?>
+$num = 10;
+$num = 10.2;
+$name = 'Johanna';
+$num = 'Hello\n$name';
+$num = "Hello\n$name";
+$num = <<<END
+Hello
+$name
+END;
+$num = true;
+$num = false;
+$num = null;
+$num = array(1, 2, 3, 4);
+$num = [1, 2, 3, 4];
+$num = new stdClass();
+var_dump($num);
