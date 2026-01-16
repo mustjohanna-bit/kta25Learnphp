c41c95ba20145359de01389eb7eecc5a444dd832
johannamust <mustjohanna@gmail.com>
2026-01-16 16:07:58 +0200

    Add Math examples

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php
 
-$num = 10;
-$num = 10.2;
-$name = 'Johanna';
-$num = 'Hello\n$name';
-$num = "Hello\n$name";
-$num = <<<END
-Hello
-$name
-END;
-$num = true;
-$num = false;
-$num = null;
-$num = array(1, 2, 3, 4);
-$num = [1, 2, 3, 4];
-$num = new stdClass();
+$num = 1;
+$num = 1 + 1;
+$num = $num + 1;
+$num += 1;
+$num -= 1;
+$num *= 2;
+$num /= 2;
+$num **= 2;
+$num %= 2;
+$num++;
+$num--;
+$num = pow(2, 10);
 var_dump($num);
