ce9e8a1e9bcb6c56afd4834ed8f0856c661033ff
johannamust <mustjohanna@gmail.com>
2026-02-11 12:58:49 +0200

    Add array examples

--- index.php
+++ index.php
@@ -1,23 +1,67 @@
 <?php
 
-$bool = true;
-$bool = false;
-$bool = true && false;
-$bool = true && true;
-$bool = true || false;
-$bool = !true;
-$bool = !false;
-$bool = 10 == 10;
-$bool = 10 == '10';
-$bool = 10 === '10';
-$bool = 10 != '10';
-$bool = 10 !== '10';
-$bool = 10 < 2;
-$bool = 10 > 2;
-$bool = 10 < 10;
-$bool = 10 >= 10;
-$bool = 10 <= 10;
-$bool = (true && false) || !(true || !false);
+$array = [1, 2, 3];
+var_dump($array[1]);
+$array[] = 4;
+$array[2] = 22;
+$array = [
+    'name' => 'Johanna', 
+    'age => 19',
+];
 
-var_dump($bool);
-false && var_dump('bla');
+var_dump($array ['name']);
+$array = [
+    1, 
+    'hello' => 'world', 
+    2,
+    3,
+    'cool' => 'stuff', 
+    '4', 
+    true, 
+    [1, 2, 3],
+    ];
+var_dump($array[5][1]);
+$array = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+];
+var_dump($array[1][2]);
+$array = [
+    [[1,2,3],
+    [4,5,6],
+    [7,8,9],[1,2,3],
+    [4,5,6],
+    [7,8,9],[1,2,3],
+    [4,5,6],
+    [7,8,9],],
+    [[1,2,3],
+    [4,5,6],
+    [7,8,9],[1,2,3],
+    [4,5,6],
+    [7,8,9],[1,2,3],
+    [4,5,6],
+    [7,8,9],],
+    [[1,2,3],
+    [4,5,6],
+    [7,8,9],[1,2,3],
+    [4,5,6],
+    [7,8,9],[1,2,3],
+    [4,5,6],
+    [7,8,9],],
+];
+var_dump($array[2][2][2]);
+
+$array = [[[[[[1]]]]]];
+var_dump($array[0][0][0][0][0][0]);
+
+$array = [
+    'apple', 
+    'cherry',
+    'pear',
+    ];
+$text = implode(',', $array);
+var_dump($text);
+
+
+
