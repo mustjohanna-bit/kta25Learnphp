3b508aa13a2150e5fa1985c47cd3e355893834ab
johannamust <mustjohanna@gmail.com>
2026-02-11 12:25:17 +0200

    Add boolean math

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ $num = [1, 2, 3, 4]; (Need on samad)
 
 - std on standard
 - sõne on string
+- $bool = !true; siin tähistab ! vastandit
+- üks = on assignment e väärtuse sättimine ja kaks == on võrdus e võrdleme kahte väärtust
+- kolmekordset === kasutatakse kui tahan et kontriollitakse ja tüüpe, strict ...; kasuta nii palju kui vähegi võimalik 
+- ! range
--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
-$num = 0.1 + 0.2;
-var_dump($num);
+$bool = true;
+$bool = false;
+$bool = true && false;
+$bool = true && true;
+$bool = true || false;
+$bool = !true;
+$bool = !false;
+$bool = 10 == 10;
+$bool = 10 == '10';
+$bool = 10 === '10';
+$bool = 10 != '10';
+$bool = 10 !== '10';
+$bool = 10 < 2;
+$bool = 10 > 2;
+$bool = 10 < 10;
+$bool = 10 >= 10;
+$bool = 10 <= 10;
+$bool = (true && false) || !(true || !false);
 
+var_dump($bool);
+false && var_dump('bla');
